876db6985338418489c539a0de7ed00c35386207
surya11231 <surya.aelvam@relevantz.com>
2024-07-23 17:58:16 +0530

    Day-2

---
+++ Day-2/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// const heading = React.createElement("h1", {id:"h1"}, "Welcome to React!");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const parent = React.createElement("div", { id: "parent" },
+    [React.createElement("div", { id: "child" }, 
+    [React.createElement("h1", { id: "h1" }, "Welcome to React!"), 
+    React.createElement("h2", { id: "h2" }, "Alu....."),]),
+    React.createElement("div", { id: "child" }, 
+    [React.createElement("h1", { id: "h1" }, "Welcome to React!"), 
+    React.createElement("h2", { id: "h2" }, "Alu....."),])],
+);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
---
+++ Day-2/README.md
@@ -0,0 +1,21 @@
+# React
+
+# Parcel 
+- Dev Build
+- Local Server
+- HMR => Hot Module Replacement
+- File Watching Algorith - written in C++
+- Catching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Diffrential Bundling - support older browers
+- Diagnostic
+- Error Handling
+- HTTP's
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
+- 
